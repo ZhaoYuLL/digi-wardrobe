c17c12de57447781a4f821a0e75f1095d65e99d7
TakekuniT <takekuni@tanemori.org>
2024-04-15 22:25:38 -0400

    added getAll function for fitposts

--- data/fitposts.js
+++ data/fitposts.js
@@ -1,1 +1,17 @@
 import { fitposts } from "../config/mongoCollections.js";
+import {ObjectId} from 'mongodb';
+
+
+const getAll = async () => {
+    const fitpostCollection = await fitposts();
+    let fpList = await fitpostCollection.find({}).toArray();
+    if (!fpList) throw 'Could not get all fitposts oop';
+    
+    // replaces ObjectId with string
+    fpList = fpList.map((element) => {
+      element._id = element._id.toString();
+      return element;
+    })
+    return fpList;
+  };
+  
