91d7d72a7b087b2e775b1419971e9e0e8c98f8db
Zhao Yu Lin <zhaoyu0305@gmail.com>
2024-04-17 16:19:17 -0400

    upload image function

--- data/fitposts.js
+++ data/fitposts.js
@@ -1,2 +1,23 @@
 import { fitposts } from "../config/mongoCollections.js";
-console.log("hi");
+import { MongoClient, GridFSBucket } from "mongodb";
+const fitpostCollection = await fitposts();
+
+// Upload an image
+const uploadImage = async (imagePath, filename, metadata) => {
+	return new Promise((resolve, reject) => {
+		const imageStream = fs.createReadStream(imagePath);
+		const uploadStream = bucket.openUploadStream(filename, {
+			contentType: "image/jpeg",
+			metadata: metadata,
+		});
+
+		imageStream
+			.pipe(uploadStream)
+			.on("error", (error) => {
+				reject(error);
+			})
+			.on("finish", () => {
+				resolve(uploadStream.id);
+			});
+	});
+};
