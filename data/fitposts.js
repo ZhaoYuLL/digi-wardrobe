d17452bd5ad129b28f6e489e85195dc1732f6e8d
TakekuniT <takekuni@tanemori.org>
2024-04-17 12:49:26 -0400

    routing for trending, latest, default, a helper js file, searchbyuserid function in fitposts.js in data

--- data/fitposts.js
+++ data/fitposts.js
@@ -1,11 +1,13 @@
 import { fitposts } from "../config/mongoCollections.js";
 import {ObjectId} from 'mongodb';
+import * as helper from '../helper.js';
+
 
 
 const getAll = async () => {
     const fitpostCollection = await fitposts();
     let fpList = await fitpostCollection.find({}).toArray();
-    if (!fpList) throw 'Could not get all fitposts oop';
+    if (!fpList) throw 'There are no fitposts';
     
     // replaces ObjectId with string
     fpList = fpList.map((element) => {
@@ -13,15 +15,26 @@ const getAll = async () => {
       return element;
     })
 
+    // fisher-yates shuffle algorithm to randomize order
+    for (let i = fpList.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [fpList[i], fpList[j]] = [fpList[j], fpList[i]];
+    }
+
     // remove later
     console.log('getAll():',fpList);
 
     return fpList;
   };
 
+
 const latest = async() => {
     let fpList = await getAll();
     fpList.sort((a, b) => new Date(b.postedDate) - new Date(a.postedDate));
+
+    // remove later
+    console.log('latest', fpList);
+
     return fpList;
 }
 
@@ -29,8 +42,25 @@ const trending = async() => {
     let fpList = await getAll();
     // some formula to find most trending
     fpList.sort((a, b) => (b.likes + b.saves) - (a.likes + a.saves));
+
+    // remove later
+    console.log('trending', fpList);
+
     return fpList;
 }
 
+const searchByUID = async(uid) => {
+    uid = helper.validString(uid);
+    let fpList = latest();
+    fpList = fpList.filter(fp => fp.user_id === uid);
+
+    if (!fpList) throw 'There are no fitposts';
+
+    // remove later
+    console.log('uid', fpList);
+
+    return fpList;
+
+}
 
 export{getAll, latest, trending}
--- routes/fitpostRoutes.js
+++ routes/fitpostRoutes.js
@@ -1,4 +1,42 @@
 import { Router } from 'express';
+import * as fp from '../data/fitposts.js';
+
 const router = Router();
+
+router.route('/').get(async (req, res) => {
+    //code here for GET will render the home handlebars file
+    try {
+        let fpList = fp.getAll();
+        res.render('explore_page', { fitposts:  fpList});
+    }
+    catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+router.route('/trending').get(async (req, res) => {
+    //code here for GET will render the home handlebars file
+    try {
+        let fpList = fp.trending();
+        res.render('explore_page', { fitposts:  fpList});
+    }
+    catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+router.route('/latest').get(async (req, res) => {
+    //code here for GET will render the home handlebars file
+    try {
+        let fpList = fp.latest();
+        res.render('explore_page', { fitposts:  fpList});
+    }
+    catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+
+
 
 export default router;
