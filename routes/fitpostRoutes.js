dd8a1920e853bcb72b1ea015a4435f5978b76f02
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-23 19:45:18 -0400

    change routing and correction of form action so that post request works

--- routes/fitpostRoutes.js
+++ routes/fitpostRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 const router = Router();
 
-//! When I use route("/fitposts") it doesn't work, since route("/") done by userRoutes, i reworded it to "/" here
-//! since the fitpostRoutes are already mounted on the /fitposts path in index.js,
-router.route("/").get(async (req, res) => {
-	res.render("fitposts", { title: "fitposts" });
-});
+//! since i appended to /fitpost in index.js, i simply start with "/" here in fitpost routes
+router
+	.route("/")
+	.get(async (req, res) => {
+		res.render("fitposts", { title: "fitposts" });
+	})
+	.post(async (req, res) => {
+		console.log(req.body);
+		res.render("fitposts", { title: "fitposts" });
+	});
 
 export default router;
