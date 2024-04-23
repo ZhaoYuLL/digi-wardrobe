ff0975453a763b083bf791e85daaddd998b71398
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-23 16:47:53 -0400

    able to render fitpost handlebars by changing route from "/fitposts" to "/"

--- routes/fitpostRoutes.js
+++ routes/fitpostRoutes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
+//! When I use route("/fitposts") it doesn't work, since route("/") done by userRoutes, i reworded it to "/" here
+//! since the fitpostRoutes are already mounted on the /fitposts path in index.js,
 router.route("/").get(async (req, res) => {
-	res.redirect("/fitposts");
-});
-router.route("/fitposts").get(async (req, res) => {
 	res.render("fitposts", { title: "fitposts" });
 });
 
