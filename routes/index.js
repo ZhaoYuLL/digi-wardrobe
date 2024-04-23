e9e98439e46097b08a3d31809cbea3d15d758e0d
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-23 16:34:20 -0400

    idk why this is in source control

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,15 @@ import outfitPieceRoutes from "./outfitPieceRoutes.js";
 import wardrobeRoutes from "./wardrobeRoutes.js";
 import { static as staticDir } from "express";
 
-
 const constructorMethod = (app) => {
-    // just basic setup, feel free to change routes
-    app.use('/fitposts', fitpostRoutes);
-    app.use('/users', userRoutes);
+	// just basic setup, feel free to change routes
+	app.use("/fitposts", fitpostRoutes);
+	app.use("/", fitpostRoutes); //change to userRoutes
 
-    app.use('/public', staticDir('public'));
-    app.use('*', (req, res) => {
-        res.redirect('/');
-    });
+	app.use("/public", staticDir("public"));
+	app.use("*", (req, res) => {
+		res.redirect("/");
+	});
 };
 
 export default constructorMethod;
