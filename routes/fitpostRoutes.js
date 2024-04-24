db8c9776b68cc5f94a74985aa9437512aa45f0e6
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-23 21:21:02 -0400

    reshape image method

--- routes/fitpostRoutes.js
+++ routes/fitpostRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
+import sharp from "sharp";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const router = Router();
 
@@ -9,6 +10,18 @@ const BUCKET_REGION = "us-east-2";
 const ACCESS_KEY = "AKIA2UC3EQYTZS4TLAVG";
 const SECRET_ACCESS_KEY = "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";
 
+const generateFileName = async (bytes = 32) => {
+	const { randomBytes } = await import("node:crypto");
+	return new Promise((resolve, reject) => {
+		randomBytes(bytes, (err, buf) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(buf.toString("hex"));
+			}
+		});
+	});
+};
 const s3 = new S3Client({
 	credentials: {
 		accessKeyId: ACCESS_KEY,
@@ -26,13 +39,15 @@ router
 		res.render("fitposts", { title: "fitposts" });
 	})
 	.post(upload.single("image"), async (req, res) => {
-		// console.log(req.body);
-		console.log("req.file", req.file);
-		req.file.buffer;
+		//resize image
+		const fileBuffer = await sharp(req.file.buffer)
+			.resize({ height: 1920, width: 1080, fit: "contain" })
+			.toBuffer();
+
 		const params = {
 			Bucket: BUCKET_NAME,
-			Key: req.file.originalname,
-			Body: req.file.buffer,
+			Key: await generateFileName(),
+			Body: fileBuffer,
 			ContentType: req.file.mimetype,
 		};
 		const command = new PutObjectCommand(params);
