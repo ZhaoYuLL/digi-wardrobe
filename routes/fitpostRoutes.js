bcb73cec329bb2abef10ebab304560b07cf08d2c
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-23 22:46:59 -0400

    ability to display images from db and s3

--- routes/fitpostRoutes.js
+++ routes/fitpostRoutes.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import multer from "multer";
 import sharp from "sharp";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { storeImage, getImage } from "../data/fitposts.js";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { storeImage, getImage, getAllImages } from "../data/fitposts.js";
 const router = Router();
 
 //not_secure
@@ -23,6 +25,7 @@ const generateFileName = async (bytes = 32) => {
 		});
 	});
 };
+
 const s3 = new S3Client({
 	credentials: {
 		accessKeyId: ACCESS_KEY,
@@ -31,13 +34,54 @@ const s3 = new S3Client({
 	region: BUCKET_REGION,
 });
 
+const addSignedUrlsToPosts = async () => {
+	try {
+		// Get all posts from the database
+		const posts = await getAllImages();
+		console.log(posts);
+		// Loop through each post and generate a signed URL for the image
+		for (let post of posts) {
+			const imageName = post.imageName;
+			// Generate a signed URL for the image
+			const signedUrl = await getSignedUrl(
+				s3,
+				new GetObjectCommand({
+					Bucket: BUCKET_NAME,
+					Key: imageName,
+				}),
+				{ expiresIn: 60 } // 60 seconds
+			);
+
+			// Add the signed URL to the post object
+			post.imageUrl = signedUrl;
+			console.log("image url:", post.imageUrl);
+		}
+
+		console.log("Signed URLs added to posts");
+		return posts;
+	} catch (error) {
+		console.error("Error adding signed URLs to posts:", error);
+		throw error;
+	}
+};
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 //! since i appended to /fitpost in index.js, i simply start with "/" here in fitpost routes
 router
 	.route("/")
 	.get(async (req, res) => {
-		res.render("fitposts", { title: "fitposts" });
+		// addSignedUrlsToPosts;
+		try {
+			console.log("yo");
+			const postsUrls = await addSignedUrlsToPosts();
+			console.log("");
+			res.render("fitposts", { title: "fitposts", posts: postsUrls });
+			// res.render("fitposts");
+		} catch (error) {
+			console.error("Error rendering fitposts:", error);
+			res.status(500).send("Internal Server Error");
+		}
 	})
 	.post(upload.single("image"), async (req, res) => {
 		//resize image
@@ -56,7 +100,7 @@ router
 		await s3.send(command);
 
 		//storing image in mongo
-		const post = await storeImage(imageName, req.body.caption);
+		const post = await storeImage(req.body.caption, imageName);
 		res.render("fitposts", { title: "fitposts" });
 	});
 
