da2a69951c461dc6b32beee958a5a1b69a902e0e
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-27 16:53:46 -0400

    routes

--- routes/wardrobeRoutes.js
+++ routes/wardrobeRoutes.js
@@ -1,20 +1,23 @@
 import { Router } from "express";
+import { getAllOutfits } from "../data/testwardrobe.js";
 const router = Router();
 
 router.get("/", (req, res) => {
 	// Render your sign-in page
-	res.render("index");
+	res.render("index", { title: "Sub-page" });
 });
 router.get("/closet", (req, res) => {
 	// Render your sign-in page
-	res.render("closet");
+	res.render("closet", { title: "Closet Page" });
 });
-router.get("/wardrobe", (req, res) => {
+router.get("/wardrobe", async (req, res) => {
 	// Render your sign-in page
-	res.render("wardrobe");
+	const outfits = await getAllOutfits();
+	console.log(outfits);
+	res.render("wardrobe", { title: "Wardrobe Page" });
 });
 router.get("/favorites", (req, res) => {
 	// Render your sign-in page
-	res.render("favorites");
+	res.render("favorites", { title: "Favorite Page" });
 });
 export default router;
