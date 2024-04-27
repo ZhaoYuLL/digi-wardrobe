379c3ccfbd84ff5357c2e5cb5cb3745823240902
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-27 14:47:58 -0400

    added start of sub pages of your page

--- routes/wardrobeRoutes.js
+++ routes/wardrobeRoutes.js
@@ -1,4 +1,20 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
 
+router.get("/", (req, res) => {
+	// Render your sign-in page
+	res.render("index");
+});
+router.get("/closet", (req, res) => {
+	// Render your sign-in page
+	res.render("closet");
+});
+router.get("/wardrobe", (req, res) => {
+	// Render your sign-in page
+	res.render("wardrobe");
+});
+router.get("/favorites", (req, res) => {
+	// Render your sign-in page
+	res.render("favorites");
+});
 export default router;
