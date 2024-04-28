16cb784e256931603cd36a57a80c9c6656400984
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-04-27 21:03:54 -0400

    able to access wardrobe data

--- routes/wardrobeRoutes.js
+++ routes/wardrobeRoutes.js
@@ -4,27 +4,29 @@ import { addSignedUrlsToFitPosts_in_wardrobe } from "../helper.js";
 const router = Router();
 
 router.get("/", (req, res) => {
-	// Render your sign-in page
-	res.render("index", { title: "Sub-page" });
+  // Render your sign-in page
+  res.render("index", { title: "Sub-page" });
 });
 router.get("/closet", (req, res) => {
-	// Render your sign-in page
-	res.render("closet", { title: "Closet Page" });
+  // Render your sign-in page
+  res.render("closet", { title: "Closet Page" });
 });
 router.get("/wardrobe", async (req, res) => {
-	// Render your sign-in page
-	const outfits = await getAllOutfits();
-	const postsWithSignedUrls = await addSignedUrlsToFitPosts_in_wardrobe(
-		outfits
-	);
-	// console.log(postsWithSignedUrls.fitposts);
-	res.render("wardrobe", {
-		title: "Wardrobe Page",
-		wardrobes: postsWithSignedUrls,
-	});
+  // Render your sign-in page
+  const outfits = await getAllOutfits();
+  const postsWithSignedUrls = await addSignedUrlsToFitPosts_in_wardrobe(
+    outfits
+  );
+  //   console.log(postsWithSignedUrls[0].wardrobeName);
+  // console.log(postsWithSignedUrls.fitposts);
+  res.render("wardrobe", {
+    title: "Wardrobe Page",
+    wardrobes: postsWithSignedUrls,
+    wardrobesJson: JSON.stringify(postsWithSignedUrls),
+  });
 });
 router.get("/favorites", (req, res) => {
-	// Render your sign-in page
-	res.render("favorites", { title: "Favorite Page" });
+  // Render your sign-in page
+  res.render("favorites", { title: "Favorite Page" });
 });
 export default router;
