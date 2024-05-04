0bf8005dcd7556ea2b39eb937f64092f56995a97
jwang14@stevens.edu <jwang14@stevens.edu>
2024-05-04 11:56:41 -0400

    fitposts1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 const router = Router();
 // import data from users 
 import { createUser, loginUser } from "../data/users.js";
-
+import { getAll, searchByFPID } from "../data/fitposts.js";
+import { addSignedUrlsToPosts } from "../helper.js";
 
 router.route('/').get(async (req, res) => {
     //code here for GET THIS ROUTE SHOULD NEVER FIRE BECAUSE OF MIDDLEWARE #1 IN SPECS.
@@ -55,7 +56,8 @@ router
               lastName: user.lastName, 
               wardrobes: user.wardrobes, 
               closet: user.closet, 
-              favorite: user.favorite
+              favorite: user.favorite,
+              fitposts: user.fitposts
             };
             res.redirect('/userProfile');
 
@@ -66,16 +68,51 @@ router
         }
     });
 
+// router.route('/userProfile').get(async (req, res) => {
+//         //code here for GET
+//     if (!req.session.user) {
+//         return res.redirect('/login');
+//     }
+//     const { username, firstName, lastName, wardrobes, closet, favorite } = req.session.user;
+//     const userName = username;
+//     const allFitpost = getAll();
+//     const postsWithSignedUrls = await addSignedUrlsToFitPosts_in_wardrobe(
+//       allFitpost
+//     );
+//   // Add signed URLs to fitposts in wardrobes
+
+//     res.render('userProfile', {userName, firstName, lastName, wardrobes: wardrobesWithSignedUrls,
+//       helpers: {displayFitpostImages,} , closet, favorite});  
+// });
+
 router.route('/userProfile').get(async (req, res) => {
-        //code here for GET
-    if (!req.session.user) {
-        return res.redirect('/login');
-    }
-    const { username, firstName, lastName, wardrobes, closet, favorite } = req.session.user;
-    const userName = username;
-    res.render('userProfile', {userName, firstName, lastName,wardrobes, closet, favorite});  
-    
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+
+  const { username, firstName, lastName, wardrobes, closet, favorite } = req.session.user;
+  const userName = username;
+
+  // Get all fitposts and add signed URLs
+  const allFitposts = await getAll();
+  const fitpostsWithSignedUrls = await addSignedUrlsToPosts(allFitposts);
+
+  // Add signed URLs to fitposts in wardrobes
+  // const wardrobesWithSignedUrls = await addSignedUrlsToFitPosts_in_wardrobe(wardrobes);
+
+  res.render('userProfile', {
+    userName,
+    firstName,
+    lastName,
+    closet,
+    favorite,
+    allFitposts: fitpostsWithSignedUrls,
+    helpers: {
+      displayFitpostImages,
+    },
+  });
 });
+
       
 router.route('/logout').get(async (req, res) => {
         //code here for GET
