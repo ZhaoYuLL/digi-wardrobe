c5206f6a079e49837d5a2e843f82227dee4db08d
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-05-06 19:30:12 -0400

    seeding

--- data/outfitPieces.js
+++ data/outfitPieces.js
@@ -2,67 +2,76 @@ import { outfitPieces } from "../config/mongoCollections.js";
 // import { outfitPieces } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 
-export const storeImage = async (caption, link, outfitType, imageName, username) => {
-	const outfitPiecesCollection = await outfitPieces();
+export const storeImage = async (
+  caption,
+  link,
+  outfitType,
+  imageName,
+  username
+) => {
+  const outfitPiecesCollection = await outfitPieces();
 
-	// Create a new document with the provided caption and imageName
-	const newImage = {
-		caption,
-		link,
-		outfitType,
-		imageName: imageName,
-		username,
-	};
+  // Create a new document with the provided caption and imageName
+  const newImage = {
+    caption,
+    link,
+    outfitType,
+    imageName: imageName,
+    username,
+  };
 
-	// Insert the new document into the outfitPieces collection
-	const result = await outfitPiecesCollection.insertOne(newImage);
+  // Insert the new document into the outfitPieces collection
+  const result = await outfitPiecesCollection.insertOne(newImage);
 
-	// Check if the insertion was successful
-	if (result.insertedId) {
-		console.log("Image stored successfully");
-		return result.insertedId;
-	} else {
-		throw new Error("Failed to store image");
-	}
+  // Check if the insertion was successful
+  if (result.insertedId) {
+    // console.log("Image stored successfully");
+    return result.insertedId;
+  } else {
+    throw new Error("Failed to store image");
+  }
 };
 
 export const getImage = async (imageName) => {
-	const outfitPiecesCollection = await outfitPieces();
+  const outfitPiecesCollection = await outfitPieces();
 
-	// Find the document with the provided imageName
-	const image = await outfitPiecesCollection.findOne({
-		imageName: imageName,
-	});
+  // Find the document with the provided imageName
+  const image = await outfitPiecesCollection.findOne({
+    imageName: imageName,
+  });
 
-	// Check if the image exists
-	if (image) {
-		console.log("Image found");
-		return image;
-	} else {
-		throw new Error("Image not found");
-	}
+  // Check if the image exists
+  if (image) {
+    // console.log("Image found");
+    return image;
+  } else {
+    throw new Error("Image not found");
+  }
 };
 
 export const getAllImages = async () => {
-	const outfitPiecesCollection = await outfitPieces();
+  const outfitPiecesCollection = await outfitPieces();
 
-	// Find all documents in the outfitPieces collection
-	const images = await outfitPiecesCollection.find({}).toArray();
+  // Find all documents in the outfitPieces collection
+  const images = await outfitPiecesCollection.find({}).toArray();
 
-	console.log(`Found ${images.length} images`);
-	return images;
+  // console.log(`Found ${images.length} images`);
+  return images;
 };
 
 export const deleteImage = async (imageName) => {
-	const outfitPiecesCollection = await outfitPieces();
-	await outfitPiecesCollection.deleteOne({ imageName: imageName });
+  const outfitPiecesCollection = await outfitPieces();
+  await outfitPiecesCollection.deleteOne({ imageName: imageName });
 };
 
 export const getOutfitPiecesByUserId = async (user_id) => {
-	// get all outfit pieces owned by user
-	const outfitPiecesCollection = await outfitPieces();
-	const userOutfitPieces = outfitPiecesCollection.find({ user_id: user_id }).toArray();
-	if (!userOutfitPieces) throw new Error(`Error getting outfit pieces for user ${user_id}`);
+  // get all outfit pieces owned by user
+  const outfitPiecesCollection = await outfitPieces();
+  const userOutfitPieces = outfitPiecesCollection
+    .find({ user_id: user_id })
+    .toArray();
+  if (!userOutfitPieces)
+    throw new Error(`Error getting outfit pieces for user ${user_id}`);
 
-	return userOutfitPieces;
-}
+  return userOutfitPieces;
+};
--- routes/outfitPieceRoutes.js
+++ routes/outfitPieceRoutes.js
@@ -4,18 +4,18 @@ import { Router } from "express";
 import multer from "multer";
 import dotenv from "dotenv";
 import {
-	s3,
-	generateFileName,
-	addSignedUrlsToPosts,
-	uploadImageToS3,
-	deleteImageFromS3,
+  s3,
+  generateFileName,
+  addSignedUrlsToPosts,
+  uploadImageToS3,
+  deleteImageFromS3,
 } from "../helper.js";
 // importing database manipulation functions
 import {
-	storeImage,
-	getImage,
-	getAllImages,
-	deleteImage,
+  storeImage,
+  getImage,
+  getAllImages,
+  deleteImage,
 } from "../data/outfitPieces.js";
 
 const router = Router();
@@ -28,54 +28,60 @@ const upload = multer({ storage: storage });
 
 //! since i appended to /fitpost in index.js, i simply start with "/" here in fitpost routes
 router
-	.route("/")
-	.get(async (req, res) => {
-		// addSignedUrlsToPosts;
-		try {
-			const postsUrls = await addSignedUrlsToPosts();
-			//this render file can be changed to what it's supposed to be. right now it
-			//is just a temp place to display the images
-			//moidfy outfitpieces.handlebars to change what's sent
-			if (req.session.user) {
-				console.log("hi");
-				console.log("user:", req.session.user);
-			} else {
-				console.log("no user");
-			}
-			res.render("outfitpieces", { title: "fitposts", posts: postsUrls, script_partial: "createOutfitPiece_script" });
-		} catch (error) {
-			console.error("Error rendering fitposts:", error);
-			res.status(500).send("Internal Server Error");
-		}
-	})
-	//upload.single uploads a single image
-	.post(upload.single("image"), async (req, res) => {
-		const imageName = await generateFileName();
-		const img = await uploadImageToS3(req.file, 1920, 1080, imageName);
+  .route("/")
+  .get(async (req, res) => {
+    // addSignedUrlsToPosts;
+    try {
+      const postsUrls = await addSignedUrlsToPosts();
+      //this render file can be changed to what it's supposed to be. right now it
+      //is just a temp place to display the images
+      //moidfy outfitpieces.handlebars to change what's sent
+      if (req.session.user) {
+        // console.log("hi");
+        // console.log("user:", req.session.user);
+      } else {
+        console.log("no user");
+      }
+      res.render("outfitpieces", {
+        title: "fitposts",
+        posts: postsUrls,
+        script_partial: "createOutfitPiece_script",
+      });
+    } catch (error) {
+      console.error("Error rendering fitposts:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  })
+  //upload.single uploads a single image
+  .post(upload.single("image"), async (req, res) => {
+    const imageName = await generateFileName();
+    console.log("decsription: ", req.body.caption);
+    console.log("imagename: ", imageName);
+    const img = await uploadImageToS3(req.file, 1920, 1080, imageName);
 
-		const post = await storeImage(
-			req.body.caption,
-			req.body.link,
-			req.body.outfitType,
-			imageName,
-			req.session.user.username
-		);
-		res.redirect("/outfitpieces");
-	});
+    const post = await storeImage(
+      req.body.caption,
+      req.body.link,
+      req.body.outfitType,
+      imageName,
+      req.session.user.username
+    );
+    res.redirect("/outfitpieces");
+  });
 router.route("/:imageName").delete(async (req, res) => {
-	try {
-		// // getting the imageName, which is the name of the image on s3 bucket
-		const s3_image_name = req.params.imageName;
+  try {
+    // // getting the imageName, which is the name of the image on s3 bucket
+    const s3_image_name = req.params.imageName;
 
-		// example usage: delete from s3, then delete from database
-		await deleteImageFromS3(s3_image_name);
-		await deleteImage(s3_image_name);
+    // example usage: delete from s3, then delete from database
+    await deleteImageFromS3(s3_image_name);
+    await deleteImage(s3_image_name);
 
-		res.send("Post deleted successfully");
-	} catch (error) {
-		console.error("Error deleting post:", error);
-		res.status(500).send("Internal Server Error");
-	}
+    res.send("Post deleted successfully");
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
